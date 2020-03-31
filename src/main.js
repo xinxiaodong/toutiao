3fecd9c505f7d27533384e16a9fcbc5d39d58868
xxd <826795224@qq.com>
2020-03-31 16:25:14 +0800

    初始化项目-安装ElementUI

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui' // 引入包
+import 'element-ui/lib/theme-chalk/index.css' // 引入样式
+
+// 注册elementui
+Vue.Vue(ElementUI)
 
 Vue.config.productionTip = false
 
