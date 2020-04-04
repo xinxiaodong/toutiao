16121001f8fbbcb1e44183d7b452b8457502c2de
xxd <826795224@qq.com>
2020-04-04 21:35:34 +0800

    主页模块-左侧导航配置路由

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -14,7 +14,10 @@
         <layout-header></layout-header>
       </el-header>
       <!-- 主要区域 -->
-      <el-main>主要区域</el-main>
+      <el-main>
+        <!-- 二级路由容器 -->
+        <router-view></router-view>
+      </el-main>
     </el-container>
   </el-container>
 </template>
