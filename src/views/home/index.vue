892e10a556fc249e8214a4bd7f75b72aeed52bab
xxd <826795224@qq.com>
2020-04-04 10:40:58 +0800

    主页模块-页面布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,20 +1,27 @@
 <template>
-  <div>
-      主页
-  </div>
+  <!-- 先放置外层容器 -->
+  <el-container>
+    <!-- 左右布局 -->
+    <el-aside>左侧</el-aside>
+    <!-- 放置外置容器 -->
+    <el-container>
+      <!-- 上下布局 -->
+      <el-header>头部</el-header>
+      <!-- 主要区域 -->
+      <el-main>主要区域</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
   name: '',
   data () {
-    return {
-    }
+    return {}
   },
   methods: {}
 }
 </script>
 
 <style lang="less" scoped>
-
 </style>
