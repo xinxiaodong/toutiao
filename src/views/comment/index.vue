ae4323ee5a8d7bb96f8c09ff05726cb1126178e7
xxd <826795224@qq.com>
2020-04-11 16:27:49 +0800

    评论列表-页面结构-table

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,7 +1,21 @@
 <template>
-  <div>
-      评论列表
-  </div>
+  <el-card>
+  <bread-crumb slot="header">
+    <!-- 插槽内容 -->
+   <template slot="title">
+        评论列表
+   </template>
+  </bread-crumb>
+  <!-- body内容 -->
+    <el-table>
+<!--  label属性 -->
+   <el-table-column width="600" label="标题"></el-table-column>
+   <el-table-column label="评论状态"></el-table-column>
+   <el-table-column label="总评论数"></el-table-column>
+   <el-table-column label="粉丝评论数"></el-table-column>
+   <el-table-column label="操作"></el-table-column>
+    </el-table>
+  </el-card>
 </template>
 
 <script>
