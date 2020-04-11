a0955e3beeb630253f4f18aa9e4e8e31cac1f523
xxd <826795224@qq.com>
2020-04-11 17:02:44 +0800

    评论列表-评论数据加载到页面及状态过滤

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -7,13 +7,20 @@
    </template>
   </bread-crumb>
   <!-- body内容 -->
-    <el-table>
+    <el-table :data="list">
 <!--  label属性 -->
-   <el-table-column width="600" label="标题"></el-table-column>
-   <el-table-column label="评论状态"></el-table-column>
-   <el-table-column label="总评论数"></el-table-column>
-   <el-table-column label="粉丝评论数"></el-table-column>
-   <el-table-column label="操作"></el-table-column>
+   <el-table-column prop="title" width="600" label="标题"></el-table-column>
+   <!-- 格式化属性 -->
+   <el-table-column :formatter="formatterBoolean" prop="comment_status" label="评论状态"></el-table-column>
+   <el-table-column prop="total_comment_count" label="总评论数"></el-table-column>
+   <el-table-column prop="fans_comment_count" label="粉丝评论数"></el-table-column>
+   <el-table-column prop="" label="操作">
+       <template slot-scope="obj">
+
+       <el-button type="text" size="small">修改</el-button>
+       <el-button type="text" size="small">{{obj.row.comment_status ? '关闭评论':'打开评论'}}</el-button>
+       </template>
+   </el-table-column>
     </el-table>
   </el-card>
 </template>
@@ -23,9 +30,32 @@ export default {
   name: '',
   data () {
     return {
+      list: []
     }
   },
-  methods: {}
+
+  methods: {
+    // 请求评论列表
+    getComment () {
+      this.$axios({
+        url: '/articles',
+        params: { response_type: 'comment' }
+      }).then(result => {
+        this.list = result.data.results // 获取评论列表数据
+      })
+    },
+    formatterBoolean (row, column, cellValue, index) {
+      // row 当前行数据
+      // column 当前列属性
+      // cellValue 当前单元格的值
+      // index 当前下标
+      return cellValue ? '正常' : '关闭'
+    }
+  },
+  created () {
+  // 调用请求数据方法
+    this.getComment()
+  }
 }
 </script>
 
