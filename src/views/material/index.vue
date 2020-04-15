774709b3b572bbbafff565b0059883204f8ba643
xxd <826795224@qq.com>
2020-04-15 17:27:13 +0800

    素材列表-分页组件及请求

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -16,14 +16,34 @@
           </el-card>
         </div>
         <!-- 全部素材内容 -->
+        <el-row type="flex" justify="center" style="height:80px" align="middle">
+          <el-pagination
+            background
+            layout="prev, pager, next"
+            :total="page.total"
+            :current-page="page.currentPage"
+            :page-size="page.pageSize"
+            @current-change="changePage"
+          ></el-pagination>
+        </el-row>
       </el-tab-pane>
       <el-tab-pane label="收藏素材" name="collect">
         <!-- 收藏素材内容 -->
         <div class="img-list">
-           <el-card class="img-card" v-for="item in list" :key="item.id">
+          <el-card class="img-card" v-for="item in list" :key="item.id">
             <img :src="item.url" alt />
-           </el-card>
+          </el-card>
         </div>
+        <el-row type="flex" justify="center" style="height:80px" align="middle">
+          <el-pagination
+            background
+            layout="prev, pager, next"
+            :total="page.total"
+            :current-page="page.currentPage"
+            :page-size="page.pageSize"
+            @current-change="changePage"
+          ></el-pagination>
+        </el-row>
       </el-tab-pane>
     </el-tabs>
   </el-card>
@@ -35,21 +55,34 @@ export default {
   data () {
     return {
       activeName: 'all', // 默认选中全部
-      list: [] // 接收全部数据
+      list: [], // 接收全部数据
+      page: {
+        // 专门放置分页数据
+        total: 0, // 数据总条数
+        pageSize: 8, // 默认每页条数
+        currentPage: 1 // 当前页码 默认第一页
+      }
     }
   },
   methods: {
+    // 切换页码
+    changePage (newPage) {
+      this.page.currentPage = newPage // 得到最新页码
+      this.getAllMaterial()
+    },
     // 切换tab事件
     changeTab () {
-
+      this.page.currentPage = 1
+      this.getAllMaterial()
     },
     // 获取所有素材
     getAllMaterial () {
       this.$axios({
         url: '/user/images',
-        params: { collect: this.activeName === 'collect' }
+        params: { collect: this.activeName === 'collect', page: this.page.currentPage, per_page: this.page.pageSize }
       }).then(result => {
         this.list = result.data.results
+        this.page.total = result.data.total_count // 图片总数
       })
     }
   },
@@ -60,27 +93,27 @@ export default {
 </script>
 
 <style lang="less" scoped>
-  .img-list{
-    display: flex;
-    flex-wrap: wrap;
-      .img-card{
-     width: 200px;
-     height: 240px;
-     margin: 20px 50px;
-     position: relative;
-     img{
-       width: 100%;
-       height: 100%;
-     }
-     .operate{
-       width: 100%;
-      font-size:20px;
-      position:absolute;
+.img-list {
+  display: flex;
+  flex-wrap: wrap;
+  .img-card {
+    width: 200px;
+    height: 240px;
+    margin: 20px 50px;
+    position: relative;
+    img {
+      width: 100%;
+      height: 100%;
+    }
+    .operate {
+      width: 100%;
+      font-size: 20px;
+      position: absolute;
       bottom: 0;
       background-color: #f4f5f6;
       height: 30px;
       left: 0;
-     }
-      }
+    }
   }
+}
 </style>
