a63d620cdcda302633dd1359c03c391df521fd99
xxd <826795224@qq.com>
2020-04-24 15:41:31 +0800

    内容列表-页面结构-搜索工具栏

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -1,8 +1,45 @@
 <template>
-  <el-card>
+  <el-card class="articles">
     <bread-crumb slot="header">
       <template slot="title">文章列表</template>
     </bread-crumb>
+    <el-row class="searchTool">
+      <el-col :span="2">
+        <span>文章状态</span>
+      </el-col>
+      <el-col :span="18">
+        <el-row type="flex" justify="start">
+          <el-radio-group>
+            <el-radio>全部</el-radio>
+            <el-radio>草稿</el-radio>
+            <el-radio>待审核</el-radio>
+            <el-radio>审核通过</el-radio>
+            <el-radio>审核失败</el-radio>
+          </el-radio-group>
+        </el-row>
+      </el-col>
+    </el-row>
+    <el-row class="searchTool">
+      <el-col :span="2">
+        <span>频道列表</span>
+      </el-col>
+      <el-col :span="18">
+        <el-select value></el-select>
+      </el-col>
+    </el-row>
+    <el-row class="searchTool">
+      <el-col :span="2">
+        <span>时间选择</span>
+      </el-col>
+      <el-col :span="18">
+        <el-date-picker
+          type="daterange"
+          range-separator="至"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+        ></el-date-picker>
+      </el-col>
+    </el-row>
   </el-card>
 </template>
 
@@ -17,4 +54,10 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.articles {
+  .searchTool {
+    height: 60px;
+    padding-left: 50px;
+  }
+}
 </style>
