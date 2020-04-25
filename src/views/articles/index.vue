3eb770123452d9f9a062b6787ce71bc7bb04a9f1
xxd <826795224@qq.com>
2020-04-25 14:07:33 +0800

    内容列表-分页请求

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -35,7 +35,7 @@
       </el-col>
       <el-col :span="18">
         <el-date-picker
-        @change="changeCondition"
+          @change="changeCondition"
           value-format="yyyy-MM-dd"
           v-model="formData.dateRange"
           type="daterange"
@@ -46,7 +46,9 @@
       </el-col>
     </el-row>
     <!-- 主体 -->
-    <el-row class="total"></el-row>
+    <el-row class="total">
+      <span>共找到{{ page.total}}条符合条件的内容</span>
+    </el-row>
     <!-- 循环的模版 -->
     <el-row
       v-for="item in list"
@@ -78,6 +80,17 @@
         </el-row>
       </el-col>
     </el-row>
+    <!-- 分页组件 -->
+    <el-row type="flex" justify="center" align="middle" style="height:60px">
+      <el-pagination
+        background
+        layout="prev, pager, next"
+        :total="page.total"
+        :current-page="page.currentPage"
+        :page-size="page.pageSize"
+        @current-change="changePage"
+      ></el-pagination>
+    </el-row>
   </el-card>
 </template>
 
@@ -93,13 +106,18 @@ export default {
       },
       channels: [], // 接收频道
       list: [], // 接收文章列表数据
-      defaultImg: require('../../assets/img/305747.jpg')
+      defaultImg: require('../../assets/img/305747.jpg'),
+      page: {
+        currentPage: 1, // 当前页码
+        pageSize: 10, // 每页条数
+        total: 1000
+      }
     }
   },
   filters: {
     // 处理显示状态
     filterStatus: function (value) {
-    // value 是过滤器前面表达式计算得到的值
+      // value 是过滤器前面表达式计算得到的值
       switch (value) {
         case 0:
           return '草稿'
@@ -132,14 +150,30 @@ export default {
     }
   },
   methods: {
+    // 改变页码
+    changePage (newPage) {
+      this.page.currentPage = newPage // 赋值当前页码
+      this.changeConditionArticle()
+    },
     // 改变条件
     changeCondition () {
+      this.page.currentPage = 1 // 强制当前页码回到第一页
       // 最新状态
+      this.changeConditionArticle()
+    },
+    // 封装方法
+    changeConditionArticle () {
       const params = {
+        page: this.page.currentPage,
+        per_page: this.page.pageSize,
         status: this.formData.status === 5 ? null : this.formData.status, // 不传为全部
         channel_id: this.formData.channel_id, // 频道
-        begin_pubdate: this.formData.dateRange.length > 0 ? this.formData.dateRange[0] : null, // 起始时间
-        end_pubdate: this.formData.dateRange.length > 1 ? this.formData.dateRange[1] : null // 截止时间
+        begin_pubdate:
+          this.formData.dateRange.length > 0
+            ? this.formData.dateRange[0]
+            : null, // 起始时间
+        end_pubdate:
+          this.formData.dateRange.length > 1 ? this.formData.dateRange[1] : null // 截止时间
       }
       this.getArticles(params)
     },
@@ -150,6 +184,7 @@ export default {
         params
       }).then(result => {
         this.list = result.data.results // 获取文章数据
+        this.page.total = result.data.total_count // 文章总数
       })
     },
     // 获取频道
