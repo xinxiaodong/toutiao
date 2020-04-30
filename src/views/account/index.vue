a875a38dc20780db34a46ab4c14c6ec3c2b35af9
xxd <826795224@qq.com>
2020-04-30 16:58:20 +0800

    账户信息-数据加载给表单

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -1,22 +1,30 @@
 <template>
   <el-card>
-      <bread-crumb slot="header">
+    <bread-crumb slot="header">
       <template slot="title">账户信息</template>
-      </bread-crumb>
-      <!-- 放置上传组件 -->
-      <el-upload class="head-upload" action=" " :show-file-list="false">
-          <img src="../../assets/img/305747.jpg" alt="">
-      </el-upload>
-      <!-- 放置组件 -->
-       <el-form style="margin-left:100px" label-width="100px">
-           <el-form-item label="用户名"><el-input style="width:30%"></el-input></el-form-item>
-           <el-form-item label="简介" style="width:30%"><el-input></el-input></el-form-item>
-           <el-form-item label="邮箱" style="width:30%"><el-input></el-input></el-form-item>
-           <el-form-item label="手机" style="width:30%"><el-input></el-input></el-form-item>
-           <el-form-item>
-               <el-button type="primary">保存信息</el-button>
-           </el-form-item>
-       </el-form>
+    </bread-crumb>
+    <!-- 放置上传组件 -->
+    <el-upload class="head-upload" action=" " :show-file-list="false">
+      <img :src="formData.photo ? formData.photo : defaultImg" alt />
+    </el-upload>
+    <!-- 放置组件 -->
+    <el-form style="margin-left:100px" label-width="100px">
+      <el-form-item label="用户名">
+        <el-input v-model="formData.name" style="width:30%"></el-input>
+      </el-form-item>
+      <el-form-item label="简介">
+        <el-input v-model="formData.intro" style="width:30%"></el-input>
+      </el-form-item>
+      <el-form-item label="邮箱">
+        <el-input v-model="formData.email" style="width:30%"></el-input>
+      </el-form-item>
+      <el-form-item label="手机">
+        <el-input v-model="formData.mobile" disabled="" style="width:30%"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary">保存信息</el-button>
+      </el-form-item>
+    </el-form>
   </el-card>
 </template>
 
@@ -25,20 +33,40 @@ export default {
   name: '',
   data () {
     return {
+      formData: {
+        name: '', // 用户名
+        intro: '', // 简介
+        photo: '', // 头像
+        email: '', // 邮箱
+        mobile: '' // 手机号
+      },
+      defaultImg: require('../../assets/img/313400.jpg')
     }
   },
-  methods: {}
+  methods: {
+    // 获取用户信息
+    getUserInfo () {
+      this.$axios({
+        url: '/user/profile'
+      }).then(result => {
+        this.formData = result.data
+      })
+    }
+  },
+  created () {
+    this.getUserInfo()
+  }
 }
 </script>
 
 <style lang="less" scoped>
-.head-upload{
-    position: absolute;
-    right: 300px;
-  img{
-      width: 200px;
-      height: 200px;
-      border-radius: 50%;
+.head-upload {
+  position: absolute;
+  right: 300px;
+  img {
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
   }
 }
 </style>
