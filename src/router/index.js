b604eae8a0b7b0ffa35587fd4b992cbde51e0f26
xxd <826795224@qq.com>
2020-05-01 16:13:25 +0800

    404页面应用

--- src/router/index.js
+++ src/router/index.js
@@ -55,6 +55,10 @@ const routes = [
     path: '/login',
     name: 'Login',
     component: Login
+  },
+  {
+    path: '*', // 匹配任何地址 但是如果其他的可以匹配 优先匹配其他 否则匹配该组件
+    component: () => import('../views/404') // 配置404页面
   }
   // {
   //   path: '/about',
