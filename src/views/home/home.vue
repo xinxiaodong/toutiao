5bff8539ee1e127c4c13d7948be2a8ed37b9a4b9
xxd <826795224@qq.com>
2020-05-01 15:53:34 +0800

    echarts在项目中的应用

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,25 +1,120 @@
 <template>
 <!-- 二级组件的默认组件 -->
   <div class="home">
-
+<div ref="myChart" class='echarts'></div>
   </div>
 </template>
 
 <script>
+import echarts from 'echarts'
 export default {
   name: '',
   data () {
     return {
     }
   },
-  methods: {}
+  methods: {},
+  mounted () {
+    const myChart = echarts.init(this.$refs.myChart) // 获取到实例对象
+    myChart.setOption(
+      {
+        title: {
+          text: '堆叠区域图'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#6a7985'
+            }
+          }
+        },
+        legend: {
+          data: ['邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎']
+        },
+        toolbox: {
+          feature: {
+            saveAsImage: {}
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            type: 'category',
+            boundaryGap: false,
+            data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ],
+        series: [
+          {
+            name: '邮件营销',
+            type: 'line',
+            stack: '总量',
+            areaStyle: {},
+            data: [120, 132, 101, 134, 90, 230, 210]
+          },
+          {
+            name: '联盟广告',
+            type: 'line',
+            stack: '总量',
+            areaStyle: {},
+            data: [220, 182, 191, 234, 290, 330, 310]
+          },
+          {
+            name: '视频广告',
+            type: 'line',
+            stack: '总量',
+            areaStyle: {},
+            data: [150, 232, 201, 154, 190, 330, 410]
+          },
+          {
+            name: '直接访问',
+            type: 'line',
+            stack: '总量',
+            areaStyle: {},
+            data: [320, 332, 301, 334, 390, 330, 320]
+          },
+          {
+            name: '搜索引擎',
+            type: 'line',
+            stack: '总量',
+            label: {
+              normal: {
+                show: true,
+                position: 'top'
+              }
+            },
+            areaStyle: {},
+            data: [820, 932, 901, 934, 1290, 1330, 1320]
+          }
+        ]
+      }
+
+    ) // 设置图标属性
+  }
 }
 </script>
 
 <style lang="less" scoped>
   .home{
-      background-image: url('../../assets/img/313400.jpg');
+      // background-image: url('../../assets/img/313400.jpg');
       height: 100vh;
       background-size: cover;
+      .echarts {
+     width:600px;
+     height: 400px;
+ }
   }
+
 </style>
