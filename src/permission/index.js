8c9dac880423b290e2267e14a15c976b60616fd1
xxd <826795224@qq.com>
2020-05-01 16:06:48 +0800

    页面进度条引入应用

--- src/permission/index.js
+++ src/permission/index.js
@@ -1,8 +1,12 @@
 // 权限拦截 导航守卫
 import router from '../router'
+import progress from 'nprogress'
+import 'nprogress/nprogress.css'
 // 全局前置守卫
 router.beforeEach(function (to, from, next) {
+  progress.start() // 开启进度条
   // 拦截谁 判断拦截地址
+
   if (to.path.startsWith('/home')) {
     // 进行权限判断 判断有无token如果有token 放过 如果没有 回到登录页
     const token = window.localStorage.getItem('user-token')
@@ -15,4 +19,7 @@ router.beforeEach(function (to, from, next) {
   } else {
     next() // 直接放行
   }
+})
+router.afterEach(function () {
+  setTimeout(() => progress.done(), 300)
 })
